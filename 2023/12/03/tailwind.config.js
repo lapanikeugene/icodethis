b14f4f97f8bd9bd1b3878c9bdb5cd48f3e410619
lapanikeugene <eugeny.lp@gmail.com>
2023-12-04 22:33:02 +0100

    complete

--- 2023/12/03/tailwind.config.js
+++ 2023/12/03/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+
+      animation: {
+        'ltr-linear-infinite': 'ltr-linear-infinite 100s linear ',
+      },
+      keyframes: {
+        'ltr-linear-infinite': {
+          'from': { 'background-position': '0 0' },
+          'to': { 'background-position': '400% 0%' },
+        },
+      },
+    },
   },
   plugins: [],
 }
